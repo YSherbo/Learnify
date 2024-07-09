82a07f725b9da037639e67c2e5f295d190de88ad
Youssef Sherbiney <150549600+YSherbo@users.noreply.github.com>
2024-07-09 14:11:51 +0300

    Version 2.0

---
+++ public/Script.js
@@ -0,0 +1,180 @@
+const GetBtn = document.getElementById("GET_Button")
+const POSTBtn = document.getElementById("POST_Button")
+const input = document.getElementById("User_Input")
+const form = document.getElementById("generateForm")
+const VideosLink = [];
+const div = document.getElementById("results")
+const fragmentText = []
+const fragment = []
+const fragmentText2 = []
+const fragment2 = []
+const input2 = []
+const fragmentText3 = []
+const fragment3 = []
+
+function create(htmlStr) {
+    var frag = document.createDocumentFragment(),
+    temp = document.createElement('div');
+    temp.innerHTML = htmlStr;
+    while (temp.firstChild) {
+        frag.appendChild(temp.firstChild);
+    }
+    return frag;
+}
+
+const URLs = ['http://localhost:4000/info', 'http://localhost:4000/', 'http://localhost:4000/Question', 'http://localhost:4000/results', 'http://localhost:4000/answers']
+
+GetBtn.addEventListener('click', GetInfo)
+POSTBtn.addEventListener('click', SendInfo)
+
+async function GetInfo(e) {
+    e.preventDefault()
+    const generating = document.createElement('h1')
+    generating.innerHTML = 'Sit Back While We Generate Your Course...'
+    div.appendChild(generating)
+
+    const res = await fetch(URLs[0], {
+        method: 'GET'
+    })
+    console.log(res);
+    const data = await res.json()
+    const videos = JSON.parse(data)
+    console.log(videos[1])
+    generating.remove()
+    for (let index = 1; index < videos.length; index++) {
+        fragmentText[index] = document.createElement('h1')
+        fragmentText[index].innerHTML = 'Video No: ' + index
+        div.appendChild(fragmentText[index])
+
+        const br = document.createElement('br')
+        div.appendChild(br)
+        
+        fragment[index] = document.createElement('iframe')
+        fragment[index].src = 'https://www.youtube.com/embed/' + videos[index]
+        fragment[index].width = 560
+        fragment[index].height = 315
+        fragment[index].title = 'YouTube video player'
+        fragment[index].frameBorder = 0
+        fragment[index].allow = 'accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share'
+        fragment[index].referrerPolicy ='strict-origin-when-cross-origin'
+        fragment[index].allowFullscreen = true
+        div.appendChild(fragment[index])
+
+        const br2 = document.createElement('br')
+        div.appendChild(br2)
+
+    }
+    const QuizBTN = document.createElement('button')
+    QuizBTN.innerHTML = 'Take Quiz'
+    div.appendChild(QuizBTN)
+    QuizBTN.addEventListener('click', GetQuestions)
+}
+
+async function GetQuestions(e) {
+    e.preventDefault()
+    const generating = document.createElement('h1')
+    generating.innerHTML = 'Generating Your Quiz...'
+    div.appendChild(generating)
+
+    const res = await fetch(URLs[2], {
+        method: 'GET'
+    })
+
+    const QuestionsJSON = await res.json()
+    const Questions = JSON.parse(QuestionsJSON)
+    console.log(Questions[1])
+
+    const QuestionElement = []
+
+    for (let index = 1; index < Questions.length; index++) {
+        QuestionElement[index] = document.createElement('h1')
+        QuestionElement[index].innerHTML = 'Question No: ' + index + '\n' + Questions[index]
+        div.appendChild(QuestionElement[index])
+
+        const br = document.createElement('br')
+        div.appendChild(br)
+
+        input2[index] = document.createElement('input')
+        input2[index].type = 'text'
+        input2[index].placeholder = 'Enter Your Answer Here'
+        div.appendChild(input2[index])
+    }
+
+
+    const submitBtn = document.createElement('button')
+    submitBtn.innerHTML = 'Submit'
+    div.appendChild(submitBtn)
+    submitBtn.addEventListener('click', SubmitInfo)
+}
+
+async function SubmitInfo(e) {
+    e.preventDefault()
+    const values = []
+    for (let index = 1; index < input2.length; index++) {
+        values[index] = input2[index].value;        
+    }
+    const res = await fetch(URLs[3], {
+        method: 'POST',
+        headers: {
+            "Content-Type": 'application/json'
+        } ,
+        body: JSON.stringify({
+            parcel: values
+        })
+
+    })
+
+    const GetAnswer = document.createElement('button')
+    GetAnswer.innerHTML = 'Get Answers'
+    div.appendChild(GetAnswer)
+    GetAnswer.addEventListener('click', GetAnswers)
+}
+
+async function GetAnswers(e) {
+    e.preventDefault()
+    const generating = document.createElement('h1')
+    generating.innerHTML = 'Getting Answers...'
+    div.appendChild(generating)
+
+    const res = await fetch(URLs[4], {
+        method: 'GET'
+    })
+    console.log(res);
+    const data = await res.json()
+    const Answers = JSON.parse(data)
+    generating.remove()
+    for (let index = 1; index < Answers.length; index++) {
+        fragmentText3[index] = document.createElement('h1')
+        fragmentText3[index].innerHTML = 'Question No: ' + index
+        div.appendChild(fragmentText3[index])
+
+        const br = document.createElement('br')
+        div.appendChild(br)
+        
+        fragment3[index] = document.createElement('h1')
+        fragment3[index].innerHTML = Answers[index]
+        fragment3[index].className = 'question'
+        div.appendChild(fragment3[index])
+
+        const br2 = document.createElement('br')
+        div.appendChild(br2)
+    }
+}
+
+
+async function SendInfo(e) {
+    e.preventDefault()
+    if(input.value == '') { return }
+    const res = await fetch(URLs[1], {
+        method: 'POST',
+        headers: {
+            "Content-Type": 'application/json'
+        } ,
+        body: JSON.stringify({
+            parcel: input.value
+        })
+
+    })
+    
+
+}
--- script.js
+++ script.js
@@ -6,14 +6,18 @@ app.use(express.static('public'));
 app.use(express.json());
 const AI = require("./Gemini_AI/AI");
 const Youtube_API = require("./Youtube_API/main");
+const { Console } = require('console');
 
 //defining data
 var User_Input = "";//What the user wants to learn
-var AI_Request = "Give Me A Learning path to learn " + User_Input + " with numbered list, no bullet points and give no examples";//sends request to AI
+var AI_Request = "Can you give me a proper roadmap to learn " + User_Input + "and please add no context and make it on a numbered list and give only one example and put it on the same line";//Request Sent To The AI Model
 var filtered_AI_Response = [];//AI Response
 var Videos_Results = [];//Videos Generated
 var Videos_Results_Filter = [];//Videos Generated
-var filtered_AI_Question = [];//AI Questions
+var Questions = [];//Questions Generated
+var keywords = [];//Keywords Generated
+var answers = [];//Answers Generated
+var CheckAnswers = []; //answers checked
 const port = process.env.PORT || 4000;
 
 function filter_AI() {
@@ -54,12 +58,24 @@ async function Finder() {
 
   for (let index = 1; index < filtered_AI_Response.length; index++) {
 
-    await Youtube_API.main(filtered_AI_Response[index] + " in " + User_Input + "Course in english");
+    await AI.run("can you give me a good search keyword to search on youtube if i want to learn " + filtered_AI_Response[index] + " in " + User_Input + " course and with no context and make it one keyword and allow spaces")
+    keywords[index] = fs.readFileSync('./result.txt', 'utf8').toString()
+
+    await Youtube_API.main(keywords[index]);
     Video_Find(index);
 
   }
 
 }
+
+async function GenerateQuestion() {
+  for (let index = 0; index < Videos_Results_Filter.length; index++) {
+        await AI.run("can You Generate Me An Open Question about the topic " + keywords[index] + " but output only the question no context just the question")
+        Questions[index] = await fs.readFileSync('./result.txt', 'utf8').toString()
+        console.log(Questions[index])//puts the question in the variable
+  }
+}
+
 
 async function Generate() {
 
@@ -72,16 +88,48 @@ async function Generate() {
 app.get('/info', (req, res) => {
   async function GenerateHTML() {
     await Generate()
-    const VideosJSON = JSON.stringify(Videos_Results_Filter)
+    const VideosJSON = await JSON.stringify(Videos_Results_Filter)
     res.status(200).json(VideosJSON)
   }
   GenerateHTML()
 })
 
+app.get('/Question', (req, res) => {
+  async function GenerateQuestionHTML() {
+    await GenerateQuestion()
+    const QuestionsJSON = JSON.stringify(Questions)
+    res.status(200).json(QuestionsJSON)
+  }
+  GenerateQuestionHTML()
+})
+
+app.get('/answers', (req, res) => {
+    res.status(200).json(JSON.stringify(CheckAnswers))
+})
+
+
+
+app.post('/results', (req, res) => {
+  const { parcel } = req.body
+  async function CheckAnswersF() {
+    answers = parcel
+    for (let index = 0; index < Questions.length; index++) {
+      await AI.run("Was " + answers[index] + " the Correct Answer for the question " + Questions[index] + "? and output only true or false and without any context")
+      CheckAnswers[index] = fs.readFileSync('./result.txt', 'utf8').toString()
+    }
+  }
+
+  if(!parcel) {
+    return res.status(400).send({ info: 'err' })
+  }
+
+  CheckAnswersF()
+})
+
 app.post('/', (req, res) => {
   const { parcel } = req.body
   User_Input = parcel
-  AI_Request = "Give Me A Learning path to learn " + User_Input + " with numbered list, no bullet points, make the example below the heading and give one single example for what i should learn";
+  AI_Request = "Can you give me a proper roadmap to learn " + User_Input + "and please add no context and make it on a numbered list and give only one example and put it on the same line";
   console.log(parcel)
   if(!parcel) {
     return res.status(400).send({ info: 'err' })
@@ -90,5 +138,5 @@ app.post('/', (req, res) => {
 })
 
 app.listen(port, () => {
-  console.log(`App Is Listening On Port` + port)
+  console.log(`Learnify Is Listening On Port : ` + port)
 })
