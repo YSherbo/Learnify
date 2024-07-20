358fe0fe7dd529af334b391559c87870a627d01f
Youssef Sherbiney <150549600+YSherbo@users.noreply.github.com>
2024-07-20 19:22:12 +0300

    Update AI.js

--- Gemini_AI/AI.js
+++ Gemini_AI/AI.js
@@ -5,27 +5,29 @@ const API_KEY = process.env.AIAPI;
 const genAI = new AI.GoogleGenerativeAI(API_KEY);
 var AI_Response = "";
 
-const safetySetting = [
-  {
-    category: AI.HarmCategory.HARM_CATEGORY_HARASSMENT,
-    threshold: AI.HarmBlockThreshold.BLOCK_NONE,
-  },
-  {
-    category: AI.HarmCategory.HARM_CATEGORY_HATE_SPEECH,
-    threshold: AI.HarmBlockThreshold.BLOCK_NONE,
-  },
-  {
-    category: AI.HarmCategory.HARM_CATEGORY_DANGEROUS_CONTENT,
-    threshold: AI.HarmBlockThreshold.BLOCK_NONE,
-  },
-  {
-    category: AI.HarmCategory.HARM_CATEGORY_UNSPECIFIED,
-    threshold: AI.HarmBlockThreshold.BLOCK_NONE,
-  },
-];
 
 async function run(question) {
-  const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash", safetySettings: safetySetting });
+  const model = genAI.getGenerativeModel({ 
+    model: "gemini-1.5-flash", 
+    safetySettings: [
+    {
+      category: AI.HarmCategory.HARM_CATEGORY_HARASSMENT,
+      threshold: AI.HarmBlockThreshold.BLOCK_NONE,
+    },
+    {
+      category: AI.HarmCategory.HARM_CATEGORY_HATE_SPEECH,
+      threshold: AI.HarmBlockThreshold.BLOCK_NONE,
+    },
+    {
+      category: AI.HarmCategory.HARM_CATEGORY_DANGEROUS_CONTENT,
+      threshold: AI.HarmBlockThreshold.BLOCK_NONE,
+    },
+    {
+      category: AI.HarmCategory.HARM_CATEGORY_UNSPECIFIED,
+      threshold: AI.HarmBlockThreshold.BLOCK_NONE,
+    },
+  ],
+  });
 
   const result = await model.generateContent(question);
   const response = await result.response;
