332e97fad642fc77bbe718b28208d5c2d104d26b
Youssef Sherbiney <150549600+YSherbo@users.noreply.github.com>
2024-07-20 19:50:43 +0300

    Update AI.js

--- Gemini_AI/AI.js
+++ Gemini_AI/AI.js
@@ -11,24 +11,24 @@ async function run(question) {
     model: "gemini-1.5-flash",
     safetySettings: [
       {
-        category: HarmCategory.HARM_CATEGORY_HARASSMENT,
-        threshold: HarmBlockThreshold.BLOCK_NONE,
+        category: AI.HarmCategory.HARM_CATEGORY_HARASSMENT,
+        threshold: AI.HarmBlockThreshold.BLOCK_NONE,
       },
       {
-        category: HarmCategory.HARM_CATEGORY_HATE_SPEECH,
-        threshold: HarmBlockThreshold.BLOCK_NONE,
+        category: AI.HarmCategory.HARM_CATEGORY_HATE_SPEECH,
+        threshold: AI.HarmBlockThreshold.BLOCK_NONE,
       },
       {
-        category: HarmCategory.HARM_CATEGORY_DANGEROUS_CONTENT,
-        threshold: HarmBlockThreshold.BLOCK_NONE,
+        category: AI.HarmCategory.HARM_CATEGORY_DANGEROUS_CONTENT,
+        threshold: AI.HarmBlockThreshold.BLOCK_NONE,
       },
       {
-        category: HarmCategory.HARM_CATEGORY_SEXUALLY_EXPLICIT,
-        threshold: HarmBlockThreshold.BLOCK_NONE,
+        category: AI.HarmCategory.HARM_CATEGORY_SEXUALLY_EXPLICIT,
+        threshold: AI.HarmBlockThreshold.BLOCK_NONE,
       },
       {
-        category: HarmCategory.HARM_CATEGORY_UNSPECIFIED,
-        threshold: HarmBlockThreshold.BLOCK_NONE,
+        category: AI.HarmCategory.HARM_CATEGORY_UNSPECIFIED,
+        threshold: AI.HarmBlockThreshold.BLOCK_NONE,
       },
     ],
   });
