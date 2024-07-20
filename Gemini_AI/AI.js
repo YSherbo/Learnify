9c04aade1906b2d681481798915df45ac76694dc
Youssef Sherbiney <150549600+YSherbo@users.noreply.github.com>
2024-07-20 17:47:01 +0300

    upgrading our gemini generative model to 1.5 flash as its much faster

--- Gemini_AI/AI.js
+++ Gemini_AI/AI.js
@@ -25,7 +25,7 @@ const safetySetting = [
 ];
 
 async function run(question) {
-  const model = genAI.getGenerativeModel({ model: "gemini-pro", safetySetting });
+  const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash", safetySetting });
 
   const result = await model.generateContent(question);
   const response = await result.response;
