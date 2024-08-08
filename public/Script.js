7da871048c7e59574dd443b81200e21b515c0149
Youssef Sherbiney <150549600+YSherbo@users.noreply.github.com>
2024-08-08 23:39:07 +0300

    fix

--- public/Script.js
+++ public/Script.js
@@ -223,7 +223,7 @@ async function GetEXP(e) {
         if (Exp[index] == "The Server Is Busy, Please Wait One Minute Then Try Again") {
             
             ExpElement[index] = document.createElement('h1')
-            ExpElement[index].innerHTML = Exp[index].split('\n').join('\n')
+            ExpElement[index].innerHTML = Exp[index].join('\n')
             div.appendChild(ExpElement[index])
         }
         else {
