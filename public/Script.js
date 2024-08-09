085a35f14f7cce7c0d34f0e920ad4534c3d4be9a
Youssef Sherbiney <150549600+YSherbo@users.noreply.github.com>
2024-08-09 16:44:10 +0300

    Update Script.js

--- public/Script.js
+++ public/Script.js
@@ -229,7 +229,7 @@ async function GetEXP(e) {
         else {
 
             ExpElement[index] = document.createElement('h1')
-            ExpElement[index].innerHTML = 'Lesson No: ' + index + '\n' + Exp[index].replace(/%/g, "\n")
+            ExpElement[index].innerHTML = 'Lesson No: ' + index + '\n' + Exp[index].replace(/%/g, "\n").replace(/"*"/g, "\n").replace(/\*/g, "\n")
             div.appendChild(ExpElement[index])
 
             const br = document.createElement('br')
